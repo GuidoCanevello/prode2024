0485a7842b9642353c4f5c5a5cc6f6509a582eb7
Et3rnauta <guidocanevello@hotmail.com>
2024-05-04 18:29:41 -0300

    Fixes

--- app.vue
+++ app.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
-const { data } = await useFetch('/api/equipos')
+let data: any = ref(undefined);
+try {
+  data.value = (await useFetch('/api/equipos')).data;
+} catch (error) {
+  console.log("Error", error)
+  data.value = error
+}
+
+let a = ref("s");
+
+let onClick = () => {
+  a.value += "d"
+}
 </script>
 
 <template>
   <v-app>
     <v-card>
+      {{ a }}
       {{ data }}
+      <v-btn color="success" @click="onClick">Hola</v-btn>
     </v-card>
   </v-app>
 </template>
