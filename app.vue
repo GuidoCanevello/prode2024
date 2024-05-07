2d835324cc57646f40feaba8b480210ceb93594e
Et3rnauta <guidocanevello@hotmail.com>
2024-05-07 18:58:41 -0300

    Fix Main Page

--- app.vue
+++ app.vue
@@ -1,25 +1,9 @@
 <script setup lang="ts">
-let data: any = ref(undefined);
-try {
-  data.value = (await useFetch('/api/equipos')).data;
-} catch (error) {
-  console.log("Error", error)
-  data.value = error
-}
 
-let a = ref("s");
-
-let onClick = () => {
-  a.value += "d"
-}
 </script>
 
 <template>
   <v-app>
-    <v-card>
-      {{ a }}
-      {{ data }}
-      <v-btn color="success" @click="onClick">Hola</v-btn>
-    </v-card>
+    <UnderConstructionCard />
   </v-app>
 </template>
---
+++ components/UnderConstructionCard.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <v-card>
+    <v-container style="text-align: center">
+      <h2>Ups! Se encuentra en desarrollo por el momento</h2>
+    </v-container>
+
+    <v-container>
+      <v-row>
+        <v-col style="text-align: center">
+          <img height="300" lazy-src="@/assets/lego-construction-worker.jpg"
+            src="/img/lego-construction-worker.jpg" />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
